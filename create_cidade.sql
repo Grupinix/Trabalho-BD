e4c807931c807efa145849b67c4f05fc366e4325
Fernando Moreira da Silva Filho <fernandomsf@id.uff.br>
2022-05-28 19:32:35 -0300

    feature: create_cidade.sql

---
+++ create_cidade.sql
@@ -0,0 +1,26 @@
+USE ESCOLA;
+
+INSERT INTO Cidade (Nome) VALUES
+    ('RIO DE JANEIRO'),
+    ('BELFORD ROXO'),
+    ('DUQUE DE CAXIAS'),
+    ('GUAPIMIRIM'),
+    ('ITABORAI'),
+    ('ITAGUAI'),
+    ('JAPERI'),
+    ('MAGE'),
+    ('MARICA'),
+    ('MESQUITA'),
+    ('NILOPOLIS'),
+    ('NITEROI'),
+    ('NOVA IGUACU'),
+    ('PARACAMBI'),
+    ('PETROPOLIS'),
+    ('QUEIMADOS'),
+    ('SEROPEDICA'),
+    ('SAO GONCALO'),
+    ('SAO JOAO DE MERITI'),
+    ('TANGUA'),
+    ('CACHOEIRAS DE MACACU'),
+    ('RIO BONITO')
+;
