4119344cbe6457c896bab19f949efb6544a9e712
yurinogueira <yurinogueira@id.uff.br>
2022-05-28 17:56:10 -0300

    feature: create schema script

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # Trabalho-BD
-Trabalho de BD 2022.1
+Trabalho de BD 2022.1 com o professor Rodrigo Salvador.
+
+### Integrantes
+- Fernando Moreira da Silva Filho
+- Rodrigo Barroso Rodrigues
+- Thiago Garcia Mozart
+- Yuri Nogueira Moreira da Silva
+
+## Lógico Relacional
+
+```
+Cidade(Codigo, Nome)
+
+Disciplina(Codigo, Nome)
+
+Pessoa(Codigo, Nome, Telefone, Codigo_Cidade)
+    Codigo_Cidade referência Cidade
+
+Professor(Codigo, RG, CPF, Titulacao)
+    Codigo referência Pessoa
+
+Escola(Codigo, Nome, Codigo_Cidade, Codigo_Professor)
+    Codigo_Cidade referência Cidade
+    Codigo_Professor referência Professor
+
+Turma(Codigo, Nome, Codigo_Escola)
+    Codigo_Escola referência Escola
+
+Aluno(Codigo, Matricula_Aluno, Data_Nascimento, Codigo_Turma)
+    Codigo referência Pessoa
+    Codigo_Turma referência Turma
+
+Contato(Nome, Codigo_Aluno, Telefone)
+    Codigo_Aluno referência Aluno
+
+Ministra(Codigo_Disciplina, Codigo_Professor)
+    Codigo_Disciplina referência Disciplina
+    Codigo_Professor referência Professor
+
+Turma_Ministra(Codigo_Disciplina, Codigo_Professor, Codigo_Turma)
+    Codigo_Disciplina, Codigo_Professor referência Ministra
+    Codigo_Turma referência Turma
+```
+
+## Criação do Esquema
+Para criação do esquema execute o seguinte script:
+[create_schema_escola.sql](create_schema_escola.sql)
---
+++ create_schema_escola.sql
@@ -0,0 +1,78 @@
+CREATE SCHEMA IF NOT EXISTS ESCOLA;
+
+USE ESCOLA;
+
+CREATE TABLE IF NOT EXISTS Cidade (
+    Codigo INTEGER AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Disciplina (
+    Codigo INTEGER AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Pessoa (
+    Codigo INTEGER AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(64) NOT NULL,
+    Telefone CHAR(16) NOT NULL,
+    Codigo_Cidade INTEGER NOT NULL,
+    FOREIGN KEY (Codigo_Cidade) REFERENCES Cidade(Codigo)
+);
+
+CREATE TABLE IF NOT EXISTS Professor (
+    Codigo INTEGER PRIMARY KEY NOT NULL,
+    RG CHAR(9) NOT NULL,
+    CPF CHAR(11) NOT NULL,
+    Titulacao VARCHAR(32) NOT NULL,
+    FOREIGN KEY (Codigo) REFERENCES Pessoa(Codigo)
+);
+
+CREATE TABLE IF NOT EXISTS Escola (
+    Codigo INTEGER AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(64) NOT NULL,
+    Codigo_Cidade INTEGER NOT NULL,
+    Codigo_Professor INTEGER NOT NULL,
+    FOREIGN KEY (Codigo_Cidade) REFERENCES Cidade(Codigo),
+    FOREIGN KEY (Codigo_Professor) REFERENCES Professor(Codigo)
+);
+
+CREATE TABLE IF NOT EXISTS Turma (
+    Codigo INTEGER AUTO_INCREMENT PRIMARY KEY,
+    Nome CHAR(16) NOT NULL,
+    Codigo_Escola INTEGER NOT NULL,
+    FOREIGN KEY (Codigo_Escola) REFERENCES Escola(Codigo)
+);
+
+CREATE TABLE IF NOT EXISTS Aluno (
+    Codigo INTEGER PRIMARY KEY NOT NULL ,
+    Matricula_Aluna CHAR(12) NOT NULL,
+    Data_Nascimento DATE NOT NULL,
+    Codigo_Turma INT NOT NULL,
+    FOREIGN KEY (Codigo_Turma) REFERENCES Turma(Codigo)
+);
+
+CREATE TABLE IF NOT EXISTS Contato (
+    Nome VARCHAR(32) NOT NULL,
+    Codigo_Aluno INTEGER NOT NULL,
+    Telefone CHAR(16) NOT NULL,
+    PRIMARY KEY (Nome, Codigo_Aluno),
+    FOREIGN KEY (Codigo_Aluno) REFERENCES Aluno(Codigo)
+);
+
+CREATE TABLE IF NOT EXISTS Ministra (
+    Codigo_Disciplina INTEGER NOT NULL,
+    Codigo_Professor INTEGER NOT NULL,
+    PRIMARY KEY (Codigo_Disciplina, Codigo_Professor),
+    FOREIGN KEY (Codigo_Disciplina) REFERENCES Disciplina(Codigo),
+    FOREIGN KEY (Codigo_Professor) REFERENCES Professor(Codigo)
+);
+
+CREATE TABLE IF NOT EXISTS Turma_Ministra (
+    Codigo_Disciplina INTEGER NOT NULL,
+    Codigo_Professor INTEGER NOT NULL,
+    Codigo_Turma INTEGER NOT NULL,
+    PRIMARY KEY (Codigo_Disciplina, Codigo_Professor, Codigo_Turma),
+    FOREIGN KEY (Codigo_Disciplina, Codigo_Professor) REFERENCES Ministra(Codigo_Disciplina, Codigo_Professor),
+    FOREIGN KEY (Codigo_Turma) REFERENCES Turma(Codigo)
+);
