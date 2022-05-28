b7ba15a6a0d0e7e1da6753f5fafa0d84277c83f5
yurinogueira <yurinogueira@id.uff.br>
2022-05-28 19:44:12 -0300

    feature: run.sh script

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+while true; do
+    read -r -p "Deseja recriar e popular o esquema ESCOLA? " sn
+
+    case $sn in
+        [Ss]* ) break;;
+        [Nn]* ) echo "Compreensível, tenha um bom dia!"; exit;;
+        * ) echo "Responda somente com [S]im ou [N]ão.";;
+    esac
+
+done
+
+read -r -p "Insira o nome do usuário MySQL/MariaDB: " user
+read -r -p "Insira a senha do usuário MySQL/MariaDB: " -s password
+
+mysql -u"$user" -p"$password" < create_schema_escola.sql
+mysql -u"$user" -p"$password" < create_cidade.sql
+mysql -u"$user" -p"$password" < create_disciplinas.sql
