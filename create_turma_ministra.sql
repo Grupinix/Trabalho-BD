6b1e2744f5918df3bfabfb588f93c6cc3e304e7a
yurinogueira <yurinogueira@id.uff.br>
2022-06-04 18:48:10 -0300

    chore: rename

--- create_turma_ministra.sql
+++ create_turma_ministra.sql
@@ -3,6 +3,6 @@ USE ESCOLA;
 INSERT INTO Turma_Ministra (Codigo_Disciplina, Codigo_Professor, Codigo_Turma)
 SELECT T_TM.Codigo_Disciplina, T_TM.Codigo_Professor, T_TM.Codigo_Turma
 FROM (
-    SELECT M.Codigo_Disciplina, M.Codigo_Professor, T.Codigo as Codigo_Turma
+    SELECT M.Codigo_Disciplina, M.Codigo_Professor, T.Codigo AS Codigo_Turma
     FROM Ministra M, Turma T ORDER BY RAND(30) LIMIT 200
 ) as T_TM;
