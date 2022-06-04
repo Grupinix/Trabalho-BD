aa7d2962347bdbd31cce625cc5f10dd2fae45fc2
yurinogueira <yurinogueira@id.uff.br>
2022-06-04 18:47:29 -0300

    chore: try to fix some weird mysql bug

--- create_turma_ministra.sql
+++ create_turma_ministra.sql
@@ -1,7 +1,8 @@
 USE ESCOLA;
 
 INSERT INTO Turma_Ministra (Codigo_Disciplina, Codigo_Professor, Codigo_Turma)
-WITH M_T AS (
+SELECT T_TM.Codigo_Disciplina, T_TM.Codigo_Professor, T_TM.Codigo_Turma
+FROM (
     SELECT M.Codigo_Disciplina, M.Codigo_Professor, T.Codigo as Codigo_Turma
     FROM Ministra M, Turma T ORDER BY RAND(30) LIMIT 200
-) SELECT * FROM M_T GROUP BY Codigo_Disciplina, Codigo_Turma;
+) as T_TM;
