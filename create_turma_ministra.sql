541175a3d34719d19e102abcf9007a11867bc890
yurinogueira <yurinogueira@id.uff.br>
2022-06-04 14:19:13 -0300

    chore: adjust query

--- create_turma_ministra.sql
+++ create_turma_ministra.sql
@@ -3,5 +3,5 @@ USE ESCOLA;
 INSERT INTO Turma_Ministra (Codigo_Disciplina, Codigo_Professor, Codigo_Turma)
 WITH M_T AS (
     SELECT M.Codigo_Disciplina, M.Codigo_Professor, T.Codigo as Codigo_Turma
-    FROM Ministra M, Turma T ORDER BY RAND()
+    FROM Ministra M, Turma T ORDER BY RAND() LIMIT 100
 ) SELECT * FROM M_T GROUP BY Codigo_Disciplina, Codigo_Turma;
